0e65d221a9742f51693737d75d2a692b746494c7
Alexandre Quintiliano <98800085+quintiliano95@users.noreply.github.com>
2024-06-21 04:04:25 -0300

    .

--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Documentos(models.Model):
-    documentos = models.CharField(max_length=100, help_text='Funcionario')
+    documentos = models.CharField(max_length=100, help_text='Documentos')
 
     def __str__(self):
         return self.documentos
