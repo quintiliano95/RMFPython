3fb6cee3d48e647a6914d7e1d94c67269f0bf78e
Alexandre Quintiliano <98800085+quintiliano95@users.noreply.github.com>
2024-06-21 04:38:11 -0300

    Bootstrap

--- RoModaFashion/settings.py
+++ RoModaFashion/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'apps.funcionarios',
     'apps.departamentos',
     'apps.documentos',
+    'apps.core',
 ]
 
 MIDDLEWARE = [
@@ -58,7 +59,7 @@ ROOT_URLCONF = 'RoModaFashion.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -107,9 +108,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
--- apps/documentos/models.py
+++ apps/documentos/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class Documentos(models.Model):
     documentos = models.CharField(max_length=100, help_text='Documentos')
+    arquivo = models.FileField(upload_to='documentos')
 
     def __str__(self):
         return self.documentos
