6984935ccadc20539628a054556474a0434c9c3a
Alexandre Quintiliano <98800085+quintiliano95@users.noreply.github.com>
2024-06-21 03:58:18 -0300

    Departamento

---
+++ apps/departamentos/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class Departamento(models.Model):
+    nome = models.CharField(max_length=100, help_text='Departamento')
+
+    def __str__(self):
+        return self.nome
+    
