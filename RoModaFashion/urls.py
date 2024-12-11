cb4f9fd9b7e83de1d68a54a3bfba50836b229eaf
Alexandre Quintiliano <98800085+quintiliano95@users.noreply.github.com>
2024-12-11 04:50:13 -0300

    login

--- RoModaFashion/urls.py
+++ RoModaFashion/urls.py
@@ -4,5 +4,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('funcionarios/', include('apps.funcionarios.urls')),
-    path('', include('apps.core.urls'))
+    path('', include('apps.core.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
